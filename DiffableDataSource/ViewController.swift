b7c612c6c8d4ed901303d5cefc16563dde486e96
SR <sarath.raveendran@litmus7@.com>
2020-07-29 23:48:18 +0530

    initial commit

---
+++ DiffableDataSource/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DiffableDataSource
+//
+//  Created by SR on 7/29/20.
+//  Copyright © 2020 SR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
